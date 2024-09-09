56dfc23d0be8fe94de7c07bcb66e96b1812fc3f9
Amruthamsh A <amruthaamsh@gmail.com>
2024-09-09 22:25:59 +0530

    fix

--- app.js
+++ app.js
@@ -17,10 +17,15 @@
  * Query for WebXR support. If there's no support for the `immersive-ar` mode,
  * show an error.
  */
-(async function() {
-  const isArSessionSupported = navigator.xr && navigator.xr.isSessionSupported && await navigator.xr.isSessionSupported("immersive-ar");
+(async function () {
+  const isArSessionSupported =
+    navigator.xr &&
+    navigator.xr.isSessionSupported &&
+    (await navigator.xr.isSessionSupported("immersive-ar"));
   if (isArSessionSupported) {
-    document.getElementById("enter-ar").addEventListener("click", window.app.activateXR)
+    document
+      .getElementById("enter-ar")
+      .addEventListener("click", window.app.activateXR);
   } else {
     onNoXRDevice();
   }
@@ -37,17 +42,18 @@ class App {
   activateXR = async () => {
     try {
       // Initialize a WebXR session using "immersive-ar".
-      this.xrSession = /* TODO */
+      this.xrSession =
+        /* TODO */
 
-      // Create the canvas that will contain our camera's background and our virtual scene.
-      this.createXRCanvas();
+        // Create the canvas that will contain our camera's background and our virtual scene.
+        this.createXRCanvas();
 
       // With everything set up, start the app.
       await this.onSessionStarted();
-    } catch(e) {
+    } catch (e) {
       onNoXRDevice();
     }
-  }
+  };
 
   /**
    * Add a canvas element and initialize a WebGL context that is compatible with WebXR.
@@ -55,10 +61,10 @@ class App {
   createXRCanvas() {
     this.canvas = document.createElement("canvas");
     document.body.appendChild(this.canvas);
-    this.gl = this.canvas.getContext("webgl", {xrCompatible: true});
+    this.gl = this.canvas.getContext("webgl", { xrCompatible: true });
 
     this.xrSession.updateRenderState({
-      baseLayer: new XRWebGLLayer(this.xrSession, this.gl)
+      baseLayer: new XRWebGLLayer(this.xrSession, this.gl),
     });
   }
 
@@ -68,16 +74,17 @@ class App {
    */
   async onSessionStarted() {
     // Add the `ar` class to our body, which will hide our 2D components
-    document.body.classList.add('ar');
+    document.body.classList.add("ar");
 
     // To help with working with 3D on the web, we'll use three.js.
     this.setupThreeJs();
 
     // Setup an XRReferenceSpace using the "local" coordinate system.
-    this.localReferenceSpace = /* TODO */;
+    this.localReferenceSpace =
+      /* TODO */
 
-    // Start a rendering loop using this.onXRFrame.
-    this.xrSession.requestAnimationFrame(this.onXRFrame);
+      // Start a rendering loop using this.onXRFrame.
+      this.xrSession.requestAnimationFrame(this.onXRFrame);
   }
 
   /**
@@ -86,7 +93,7 @@ class App {
    */
   onXRFrame = (time, frame) => {
     /** TODO draw the application */
-  }
+  };
 
   /**
    * Initialize three.js specific rendering code, including a WebGLRenderer,
@@ -99,7 +106,7 @@ class App {
       alpha: true,
       preserveDrawingBuffer: true,
       canvas: this.canvas,
-      context: this.gl
+      context: this.gl,
     });
     this.renderer.autoClear = false;
 
@@ -112,6 +119,6 @@ class App {
     this.camera = new THREE.PerspectiveCamera();
     this.camera.matrixAutoUpdate = false;
   }
-};
+}
 
 window.app = new App();
